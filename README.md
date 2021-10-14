2bc2c5fb81b9c5fb804dc3dc2ac7ce4bd0fb6864
odehabuzaid <odeh.abuzaid@outlook.com>
2021-10-14 23:26:21 +0300

    LinkedLists

--- README.md
+++ README.md
@@ -9,4 +9,4 @@
 |     1    | [Array-reverse](python/code_challenges/array-reverse/README.md)
 |     2    | [Array-insert-shift](python/code_challenges/array-insert-shift/README.md)
 |     3    | [array-binary-search](python/code_challenges/array-binary-search/README.md)
-
+|     4    | [Linked Lists](python/linked_list/README.md)
--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -1,5 +1,59 @@
-from linked_list.linked_list import LinkedList
+# import pytest
+
+# from linked_list import LinkedList, Node
 
 
-def test_import():
-    assert LinkedList
+# def test_node_has_int_data():
+#     # Arrange any data that you need to run your test
+#     expected = 1
+
+#     # Act on the subject of the test to produce some actual output
+#     node = Node(1)
+#     actual = node.data
+
+#     # Assert
+#     assert actual == expected
+
+
+# def test_node_has_str_data():
+#     # Arrange any data that you need to run your test
+#     expected = "a"
+
+#     # Act on the subject of the test to produce some actual output
+#     node = Node("a")
+#     actual = node.data
+
+#     # Assert
+#     assert actual == expected
+
+
+# def test_node_is_a_Node():
+#     # Arrange any data that you need to run your test
+#     expected = "Node"
+
+#     # Act on the subject of the test to produce some actual output
+#     node = Node(1)
+#     actual = type(node).__name__
+
+#     # Assert
+#     assert actual == expected
+
+# def test_node_without_value():
+#   with pytest.raises(TypeError):
+#     node = Node()
+
+
+# def test_new_linked_list_is_empty():
+#   expected = None
+
+#   ll = LinkedList()
+#   actual = ll.head
+
+#   assert actual == expected
+
+# def test_linked_list_insert():
+#   # Arrange
+#   expected = 1
+#   ll = LinkedList()
+#   # Act
+#   actual = ll.insert()
