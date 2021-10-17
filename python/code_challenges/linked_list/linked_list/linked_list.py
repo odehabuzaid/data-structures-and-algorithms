0c51592c193f9b13b4dcc7fea9e166c63809007a
Odeh <abuzaid163@hotmail.com>
2021-10-17 23:52:00 +0300

    Solution

--- python/code_challenges/linked_list/linked_list/linked_list.py
+++ python/code_challenges/linked_list/linked_list/linked_list.py
@@ -1,128 +1,180 @@
 class Node:
-  """
-  A class representing a Node
-  Attributes
-  ----------
-  
-  Methods
-  -------
-  __init__(data, next_):
-  the constructor method for the class, 
-  it takes two parameters, 
-  the data parameter is the a reference to the data the node will hold, 
-  and the next_ parameter is a reference to the next node .
-  """
-  def __init__(self, data):
-      self.data = data
-      self.next = None
-  def __str__(self):
-    return '%s' % self.data
-  
+    """
+    A class representing a Node
+    Attributes
+    ----------
+    Methods
+    -------
+    __init__(data, next_):
+    the constructor method for the class,
+    it takes two parameters,
+    the data parameter is the a reference to the data the node will hold,
+    and the next_ parameter is a reference to the next node .
+    """
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.previous = None
+
+    def __str__(self):
+        return '%s' % self.data
+
 class LinkedList:
-  """
-  A class for creating instances of a Linked List.
-  
-  Methods
-  -------
-  __init__(self, nodes):the constructor method for the class, 
-  __repr__(self): string representation
-  __iter__(self): yields every single node.
-  
-  insert(value: any) -> non
-  contains(value: any) -> bool
-  """
-  
-  def __init__(self, nodes=None):
     """
-    instantiation, an empty Linked List will be created.
+    A class for creating instances of a Linked List.
+    Methods
+    -------
+    __init__(self, nodes):the constructor method for the class,
+    __repr__(self): string representation
+    __iter__(self): yields every single node.
+    insert(value: any) -> non
+    contains(value: any) -> bool
+    """
+    def __init__(self):
+        """
+        instantiation, an empty Linked List will be created.
 
-    Args:
-        nodes (any, optional): Defaults to None.
-    """
-    self.head = None
-    if nodes is not None:
-        node = Node(data=nodes.pop(0))
-        self.head = node
-        for elem in nodes:
-            node.next = Node(data=elem)
+        Args:
+            nodes (any, optional): Defaults to None.
+        """
+        self.head = None # Initally there are no elements in the list
+        self.tail = None
+    def __repr__(self):
+        """
+        a string representing all the values in the Linked List
+
+        Returns:
+            string: formated as  "{ a } -> { b } -> { c } -> NULL"
+        """
+        node = self.head
+        nodes = []
+        while node is not None:
+            nodes.append('{%s}' % node.data)
             node = node.next
-              
-  def __repr__(self):
-    """
-    a string representing all the values in the Linked List
-    
-    Returns:
-        string: formated as  "{ a } -> { b } -> { c } -> NULL"
-    """
-    node = self.head
-    nodes = []
-    while node is not None:
-        nodes.append('{%s}' % node.data)
-        node = node.next
-    nodes.append("Null")
-    return " -> ".join(nodes)
+        nodes.append("Null")
+        return " -> ".join(nodes)
+    def __iter__(self):
+        """
+        going through every single node,
+        starting with the head of the linked list and
+        ending on the node that has a next value of None.
 
-  
-  def __iter__(self):
-    """
-    going through every single node, 
-    starting with the head of the linked list and 
-    ending on the node that has a next value of None.
+        yields every single node.
+        """
+        if self.head is None and self.tail is None:
+            raise Exception("List is empty")
+        node = self.head
+        while node is not None:
+            yield node
+            node = node.next
+    def insert(self, value=1):
+        """
+        Adds a new node with that value to the head of the list with an O(1) Time performance.
 
-    yields every single node.
-    """
-    if self.head is None:
-      raise Exception("List is empty")
-    
-    node = self.head
-    while node is not None:
-        yield node
-        node = node.next
-   
-  def insert(self, node=Node(1)):
-    """
-    Adds a new node with that value to the head of the list with an O(1) Time performance.
+        arguments: value : any
+        returns: None
+        """
 
-    arguments: value : any
-    returns: None
-    """
-    node.next = self.head
-    self.head = node
-  
-  
-  def includes(self, data):
-    """
-    Indicates whether that value exists as a Node’s value somewhere within the list.
-    
-    Arguments: value
-    Returns: Boolean
-    """
-    if self.head is None:
-      raise Exception("List is empty")
+        node = Node(value)
+        node.next = self.head
+        if self.head != None:
+            self.head.previous = node
+            self.head = node
+            node.previous = None
 
-    for node in self:
-        if node.data == data:
-            return True
-    else: 
-      return False
-  
-  def collect(self):
-    """
-    going through every single node, 
-    starting with the head of the linked list and 
-    ending on the node that has a next value of None.
+        else:
+            self.head = node
+            self.tail = node
+            node.previous = None
+    def append(self, value=1):
+        node = Node(value)
+        node.previous = self.tail
+        if self.tail == None:
+            self.head = node
+            self.tail = node
+            node.next = None
+        else:
+            self.tail.next = node
+            node.next = None
+            self.tail = node
 
-    appends nodes data to a list 
-    
-    returns a collection of all the values exists
-    """
-    if self.head is None:
-      raise Exception("List is empty")
+    def insert_after(self, after_node, value):
+        node = self.get_node(after_node)
+        new_node = Node(value)
 
-    collection = []
-    node = self.head
-    while node is not None:
-        collection.append(node.data)
-        node = node.next
-    
-    return collection
+        new_node.next = node.next
+        node.next = new_node
+        new_node.previous = node
+        if new_node.next != None:
+            new_node.next.previous = new_node
+        # checks if new node is being added to the last element nad makes new node the new tail
+        if node == self.tail:
+            self.tail = new_node
+    def insert_before(self, before_node, value):
+        node = self.get_node(before_node)
+        new_node = Node(value)
+
+        new_node.previous = node.previous
+        node.previous = new_node
+        new_node.next = node
+
+        if new_node.previous != None:
+            new_node.previous.next = new_node
+
+        if node == self.head:
+            self.head = new_node
+    def includes(self, data):
+        """
+        Indicates whether that value exists as a Node’s value somewhere within the list.
+
+        Arguments: value
+        Returns: Boolean
+        """
+        if self.head is None:
+            raise Exception("List is empty")
+
+        for node in self:
+            if node.data == data:
+                return True
+        else:
+            return False
+    def collect(self):
+        """
+        going through every single node,
+        starting with the head of the linked list and
+        ending on the node that has a next value of None.
+
+        appends nodes data to a list
+
+        returns a collection of all the values exists
+        """
+        if self.head is None:
+            raise Exception("List is empty")
+
+        collection = []
+        node = self.head
+        while node is not None:
+            collection.append(node.data)
+            node = node.next
+
+        return collection
+    def delete(self, value):
+        if self.includes(value):
+            arr = self.collect()
+            arr.remove(value)
+            self.delete_all_nodes()
+            for i in arr:self.insert(i)
+        else:
+            raise Exception('Value is not in the list')
+    def get_node(self, value):
+        current = self.head
+        while (current):
+            if (current.data == value):
+                return current
+            current = current.next
+    def delete_all_nodes(self):
+        while (self.head != None):
+            self.head = self.head.next
+
+
+
--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -2,67 +2,65 @@ import pytest
 
 from code_challenges.linked_list.linked_list.linked_list import LinkedList, Node
 
+linkedlist = LinkedList()
 
 # Can successfully instantiate an empty linked list
 def test_new_linked_list_is_empty():
   # Arrange
   expected = None
   # Act
-  ll = LinkedList()
-  actual = ll.head
+  nll = LinkedList()
+  actual = nll.head
   # Assert
   assert actual == expected
 
-# Can properly insert into the linked list
+# Can properly append into the linked list
 def test_linked_list_insert_into_linkedlist():
   # Arrange
   expected = True
-  ll = LinkedList()
   # Act
-  ll.insert(Node(1))
+  actual = linkedlist.includes(1)
   # Assert
-  assert ll.includes(1) == expected
+  assert actual == expected
 
 # The head property will properly point to the first node in the linked list
 def test_linked_list_head_point_the_first_node():
   # Arrange
-  expected = 'paytho1'
+  expected = '9'
   # Act
-  ll = LinkedList()
-  ll.insert(Node('python'))
-  ll.insert(Node('paytho1'))
-  actual = ll.head.__str__()
+  actual = linkedlist.head.__str__()
   # Assert
   assert expected == actual
 
-# Can properly insert multiple nodes into the linked list
+# Can properly append multiple nodes into the linked list
 def test_linked_list_insert_multible_nodes():
   # Arrange
-  ll = LinkedList()
-  expected = '{c} -> {b} -> {a} -> Null'
+
+  multiple_node_ll = LinkedList()
+
+  expected = '{a} -> {b} -> {c} -> Null'
   # Act
   node1 = Node("a") # {a} -> Null
   node2 = Node("b") # {b} -> Null
   node3 = Node("c") # {c} -> Null
-  
-  node1.next =  node3  
+
+  node1.next =  node3
   node2.next =  node1
-  
-  ll.insert(node1)
-  ll.insert(node2)
-  ll.insert(node3)
-  
+
+  multiple_node_ll.append(node1)
+  multiple_node_ll.append(node2)
+  multiple_node_ll.append(node3)
+
   # Assert
-  assert expected == ll.__str__()
-  
+  assert expected == multiple_node_ll.__str__()
+
 # Will return true when finding a value within the linked list that exists
 def test_linked_list_includes_true():
   # Arrange
   expected = True
   # Act
-  ll = LinkedList()
-  ll.insert(Node('python'))
-  actual = ll.includes('python')
+  linkedlist.append('python')
+  actual = linkedlist.includes('python')
   # Assert
   assert expected == actual
 
@@ -71,55 +69,42 @@ def test_linked_list_includes_false():
   # Arrange
   expected = False
   # Act
-  ll = LinkedList()
-  ll.insert(Node('python'))
-  actual = ll.includes('python1')
+  actual = linkedlist.includes('python1')
   # Assert
   assert expected == actual
 
 # Can properly return a collection of all the values that exist in the linked list
 def test_linked_list_return_collection():
   # Act
-  ll = LinkedList()
-  ll.insert(Node('python'))
-  ll.insert(Node('data'))
-  ll.insert(Node([1,2,3]))
-  actual = ll.collect()
+  actual = linkedlist.collect()
   # Assert
   assert type(actual) is list
-
 
 
 def test_node_has_int_data():
     # Arrange any data that you need to run your test
     expected = 1
-    
     # Act on the subject of the test to produce some actual output
     node = Node(1)
     actual = node.data
-
     # Assert
     assert actual == expected
 
 def test_node_has_str_data():
     # Arrange any data that you need to run your test
     expected = "a"
-
     # Act on the subject of the test to produce some actual output
     node = Node("a")
     actual = node.data
-
     # Assert
     assert actual == expected
 
 def test_node_is_a_node():
     # Arrange any data that you need to run your test
     expected = "Node"
-
     # Act on the subject of the test to produce some actual output
     node = Node(1)
     actual = type(node).__name__
-
     # Assert
     assert actual == expected
 
@@ -130,11 +115,91 @@ def test_node_without_value():
 def test_linked_list_insert():
   # Arrange
   expected = 1
-  ll = LinkedList()
+  linkedlist = LinkedList()
   # Act
-  actual = ll.insert()
+  actual = linkedlist.append()
   # Assert
 
 
+# Can successfully add a node to the end of the linked list
+# Can successfully add multiple nodes to the end of a linked list
+# Can successfully insert a node before a node located i the middle of a linked list
+# Can successfully insert a node before the first node of a linked list
+# Can successfully insert after a node in the middle of the linked list
+# Can successfully insert a node after the last node of the linked list
+def test_linked_list_append():
+    # Arrange
+    expected = 'END'
+    # Act
+    linkedlist.append('END1')
+    linkedlist.append('END')
+    values = linkedlist.collect()
+    actual = values[len(values)-1]
+    # Assert
+    assert expected == actual
+
+def test_linked_list_insert_before():
+    # Arrange
+    expected = '44'
+    # Act
+    linkedlist.insert_before(5,'44') # 5 at index 5
+    values = linkedlist.collect()
+    actual = values[4]
+    # Assert
+    assert expected == actual
+
+def test_linked_list_insert_before_first_node():
+    # Arrange
+    expected = [1,2,3]
+    # Act
+    linkedlist.insert([1,2,3])
+    values = linkedlist.collect()
+    actual = values[0]
+    # Assert
+    assert expected == actual
+
+def test_linked_list_insert_after():
+    # Arrange
+    expected = 'after'
+    linkedlist.insert_after(9,'after') # 9 at index 0
+    # Act
+    values = linkedlist.collect()
+    actual = values[1]
+    # Assert
+    assert expected == actual
+
+def test_linked_list_insert_after_last_node():
+    # Arrange
+    expected = 'last Node'
+    linkedlist.append('last Node')
+    # Act
+    values = linkedlist.collect()
+    actual = values[len(values)-1]
+    # Assert
+    assert expected == actual
 
 
+
+@pytest.fixture(autouse=True)
+def clean():
+    linkedlist.delete_all_nodes()
+    for i in range(10):
+        linkedlist.insert(i)
+
+######################
+#Stretch
+######################
+def test_linked_list_delete_value():
+    # Arrange
+    expected = 8
+    # Act
+    linkedlist.delete(9)
+    values = linkedlist.collect()
+    actual = values[len(values)-1]
+    # Assert
+    assert expected == actual
+
+# def test_linked_list_delete_value_not_in_list():
+#     with pytest.raises(Exception) as e:
+#         linkedlist.delete('Node')
+#     assert e == 'Value is not in the list'
