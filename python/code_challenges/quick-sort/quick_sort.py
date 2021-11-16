b0a26895ba4632209555a2567ac7f12caa4e8714
Odeh <abuzaid163@hotmail.com>
2021-11-17 00:34:40 +0200

    feat(quick-sort): functionality working correctly ci : pass

--- python/code_challenges/quick-sort/quick_sort.py
+++ python/code_challenges/quick-sort/quick_sort.py
@@ -1,2 +1,31 @@
-def quick_sort(lst: list):
+def quick_sort(lst: list, left: int = 0, right=None):
+    if right == None:
+        right = len(lst)-1
+        quick_sort(lst, left, right)
+
+    def swap(index, low):
+        temp = lst[index]
+        lst[index] = lst[low]
+        lst[low] = temp
+
+    def partition(left, right):
+        pivot = lst[right]
+        low = left - 1
+        for i in range(left, right):
+            if lst[i] <= pivot:
+                low += 1
+                swap(i, low)
+
+        swap(right, low + 1)
+        return low + 1
+
+    if left < right:
+        position = partition(left, right)
+        quick_sort(lst, left, position-1)
+
+        quick_sort(lst, position + 1, right)
+    
     return lst
+
+
+print(quick_sort([8, 4, 23, 42, 16, 15]))
